d5a53e0b3c5c89962bb01bc0aa9a51795d3d9414
MSR-Enjoyer <babbibble@gmail.com>
2024-08-06 16:11:53 -0600

    Tweak to removals

--- groovy/postInit/removals.groovy
+++ groovy/postInit/removals.groovy
@@ -1,4 +1,4 @@
 
-crafting.remove('gregtech:gt_armor', 2)
-crafting.remove('gregtech:gt_armor', 3)
-crafting.remove('gregtech:gt_armor', 4)
+crafting.removeByOutput('gregtech:gt_armor', 2)
+crafting.removeByOutput('gregtech:gt_armor', 3)
+crafting.removeByOutput('gregtech:gt_armor', 4)
