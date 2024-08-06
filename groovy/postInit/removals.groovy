81b9eafd219b1152fc531a9f4dd2e03c3e4da082
MSR-Enjoyer <babbibble@gmail.com>
2024-08-06 16:26:01 -0600

    removals.groovy no longer tries to use IIngredient

--- groovy/postInit/removals.groovy
+++ groovy/postInit/removals.groovy
@@ -1,4 +1,4 @@
 
-crafting.removeByOutput('gregtech:gt_armor', 2)
-crafting.removeByOutput('gregtech:gt_armor', 3)
-crafting.removeByOutput('gregtech:gt_armor', 4)
+crafting.remove('gregtech:electric_jetpack_advanced')
+crafting.remove('gregtech:electric_jetpack')
+crafting.remove('gregtech:fluid_jetpack')
