4481ad790b88e2692f50ee12f49828e1854adb4f
trainvoi <voitrainelephant@gmail.com>
2024-08-20 20:31:59 +0700

    let there be desh
    commit not yet tested

--- groovy/preInit/materials.groovy
+++ groovy/preInit/materials.groovy
@@ -1,9 +1,18 @@
-import gregtech.api.unification.material.event.MaterialEvent
-import gregtech.api.unification.material.Material
+
+
+import gregtech.api.unification.material.Material;
+import gregtech.api.GregTechAPI;
+import gregtech.api.fluids.attribute.FluidAttributes;
+import gregtech.api.fluids.FluidBuilder;
+import gregtech.api.unification.material.properties.*
+
+import static gregtech.api.unification.material.info.MaterialIconSet.*;
+import static gregtech.api.unification.material.info.MaterialFlags.*;
+import static gregtech.api.unification.material.Materials.*;
+import gregtech.api.unification.material.properties.BlastProperty.GasTier;
+import static gregtech.api.fluids.FluidConstants.*;
 
 import net.minecraft.util.ResourceLocation
-
-import static gregtech.api.unification.material.info.MaterialFlags.*
 
 event_manager.listen { MaterialEvent event ->
     new Material.Builder(32000, resource('gcp', 'fluix'))
@@ -12,6 +21,12 @@ event_manager.listen { MaterialEvent event ->
             .flags('generate_plate', 'disable_decomposition', 'no_smelting', 'crystallizable')
             .components(material('certus_quartz'), material('nether_quartz'), material('redstone'))
             .build()
+
+    new Material.Builder(32001, resource('gcp', 'desh'))
+            .ingot()
+            .color(0xC40000).iconSet('METALLIC')
+            .build()
+
 
     material('certus_quartz').addFlags('generate_rod', 'generate_bolt_screw')
     material('nether_quartz').addFlags('generate_rod', 'generate_bolt_screw')
