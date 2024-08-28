f00ddcc4cce887fca1a2d3acd9997a58b9234ec4
last8kings <97848034+last8kings@users.noreply.github.com>
2024-08-28 14:16:53 -0400

    buff steam

--- groovy/prePostInit/Thermodynamics.groovy
+++ groovy/prePostInit/Thermodynamics.groovy
@@ -1,1 +1,17 @@
+package prePostInit;
 
+import classes.*;
+import globals.Globals;
+
+import java.lang.Math;
+
+//REMOVALS
+
+mods.gregtech.steam_turbine.removeByInput(32, null, [fluid('steam') * 640])
+
+
+ recipemap('steam_turbine').recipeBuilder()
+            .fluidInputs(fluid('steam') * 480)
+            .duration(10)
+            .EUt(32)
+            .buildAndRegister()
