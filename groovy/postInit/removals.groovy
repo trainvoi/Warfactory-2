c1ea036c89836179a49be5dfa12d8d863157bf1c
last8kings <97848034+last8kings@users.noreply.github.com>
2024-09-01 07:21:12 -0400

    remove old multismelter recipe

--- groovy/postInit/removals.groovy
+++ groovy/postInit/removals.groovy
@@ -2,3 +2,4 @@
 crafting.remove('gregtech:electric_jetpack_advanced')
 crafting.remove('gregtech:electric_jetpack')
 crafting.remove('gregtech:fluid_jetpack')
+crafting.remove('gregtech:multi_furnace')
